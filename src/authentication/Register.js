4f4e6c36784703ec774ab80b66fbdbf7787fd0aa
gars150387 <gar.santeliz@gmail.com>
2022-12-06 14:28:41 -0500

    admin user role update function set up

--- src/authentication/Register.js
+++ src/authentication/Register.js
@@ -3,13 +3,14 @@ import { apiBase } from "../components/api/Api";
 import { useForm } from "../hooks/useForm";
 import "../style/component/Login.css";
 import { useNavigate } from "react-router";
+import { UpdateUser } from "./UpdateUser";
 
 const registerFormFields = {
   registerName: "",
   registerEmail: "",
   registerPassword: "",
   registerPassword2: "",
-  registerRole:""
+  registerRole: "",
 };
 
 export const Register = () => {
@@ -23,102 +24,114 @@ export const Register = () => {
     onInputChange: onRegisterInputChange,
   } = useForm(registerFormFields);
 
-  const onSubmitRegister = async(event) => {
+  const onSubmitRegister = async (event) => {
     event.preventDefault();
-try {
-  if (registerPassword !== registerPassword2) {
-      Swal.fire("error", "Passwords must match", "error");
-      return;
-    } else {
-      const response = await apiBase.post("/admin/new_admin_user", {
-        name: registerName,
-        email: registerEmail.toLowerCase(),
-        password: registerPassword,
-        role: registerRole,
-      });
-      if( response ) {
-      navigate("/pricing");
+    try {
+      if (registerPassword !== registerPassword2) {
+        Swal.fire("error", "Passwords must match", "error");
+        return;
+      } else {
+        const response = await apiBase.post("/admin/new_admin_user", {
+          name: registerName,
+          email: registerEmail.toLowerCase(),
+          password: registerPassword,
+          role: registerRole,
+        });
+        if (response) {
+          navigate("/pricing");
+        }
       }
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: Register.js ~ line 34 ~ onSubmitRegister ~ error",
+        error
+      );
+      Swal.fire("error", `${error.response.data.msg}`, "error");
     }
-} catch (error) {
-  console.log("🚀 ~ file: Register.js ~ line 34 ~ onSubmitRegister ~ error", error)
-  Swal.fire("error", `${error.response.data.msg}`, "error")
-}
-    
   };
 
   return (
-    <div className="container login-container">
-      <div className="row">
-        <div className="col-md-6 login-form-2">
-          <h3>Register</h3>
-          <form onSubmit={onSubmitRegister}>
-            <div className="form-group mb-2">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Name"
-                name="registerName"
-                value={registerName}
-                onChange={onRegisterInputChange}
-              />
-            </div>
-            <div className="form-group mb-2">
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Email"
-                name="registerEmail"
-                value={registerEmail}
-                onChange={onRegisterInputChange}
-              />
-            </div>
-            <div className="form-group mb-2">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-                name="registerPassword"
-                value={registerPassword}
-                onChange={onRegisterInputChange}
-              />
-            </div>
-            <div className="form-group mb-2">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Repite password"
-                name="registerPassword2"
-                value={registerPassword2}
-                onChange={onRegisterInputChange}
-              />
-            </div>
-            <div className="form-group mb-2">
-              <select
-                type="text"
-                className="form-control"
-                placeholder="Permiso"
-                name="registerRole"
-                // value={registerRole}
-                onChange={onRegisterInputChange}
-              >
-                <option defaultValue>Selecciona el role</option>
-                <option value="Vendedor">Vendedor</option>
-                <option value="Encargado">Encargado</option>
-                <option value="Administrador">Administrador</option>
-              </select>
-            </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        <div className="login-container">
+            <div className="login-form-2">
+              <h3>Register</h3>
+              <form onSubmit={onSubmitRegister}>
+                <div className="form-group mb-2">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Name"
+                    name="registerName"
+                    value={registerName}
+                    onChange={onRegisterInputChange}
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Email"
+                    name="registerEmail"
+                    value={registerEmail}
+                    onChange={onRegisterInputChange}
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Password"
+                    name="registerPassword"
+                    value={registerPassword}
+                    onChange={onRegisterInputChange}
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Repite password"
+                    name="registerPassword2"
+                    value={registerPassword2}
+                    onChange={onRegisterInputChange}
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <select
+                    type="text"
+                    className="form-control"
+                    placeholder="Permiso"
+                    name="registerRole"
+                    // value={registerRole}
+                    onChange={onRegisterInputChange}
+                  >
+                    <option defaultValue>Selecciona el role</option>
+                    <option value="Vendedor">Vendedor</option>
+                    <option value="Encargado">Encargado</option>
+                    <option value="Administrador">Administrador</option>
+                  </select>
+                </div>
 
-            <div className="d-grid gap-2">
-              <input
-                type="submit"
-                className="btnSubmit"
-                value="Register user"
-              />
+                <div className="d-grid gap-2">
+                  <input
+                    type="submit"
+                    className="btnSubmit"
+                    value="Register user"
+                  />
+                </div>
+              </form>
             </div>
-          </form>
         </div>
+        <UpdateUser />
       </div>
-    </div>
+    </>
   );
 };
---
+++ src/authentication/UpdateUser.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import Swal from "sweetalert2";
+import { apiBase } from "../components/api/Api";
+import "../style/component/UpdateAdminUserRole.css";
+
+export const UpdateUser = () => {
+  const [userEmail, setUserEmail] = useState("");
+  console.log("🚀 ~ file: UpdateUser.js:8 ~ UpdateUser ~ userEmail", userEmail)
+  const [role, setRole] = useState("");
+  console.log("🚀 ~ file: UpdateUser.js:10 ~ UpdateUser ~ role", role)
+
+  const onUpdateUserRole = async () => {
+    try {
+        const response = await apiBase.put("/admin/update-role", {
+            userEmail,
+            role
+        })
+        console.log("🚀 ~ file: UpdateUser.js:19 ~ onUpdateUserRole ~ response", response)
+        if (response){
+            Swal.fire("Actiaizado!", "Permiso actualizado", "success")
+        }
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: Register.js ~ line 34 ~ onSubmitRegister ~ error",
+        error
+      );
+      Swal.fire("error", `${error.response.data.msg}`, "error");
+    }
+  };
+
+  return (
+    <div className="updateAdminRole-container">
+        <h3> Actualiza Admin Usuario Permiso</h3>
+        <form className="updateAdminRole-form">
+          <form onSubmit={onUpdateUserRole}>
+            <div className="form-group mb-2">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                name="userEmail"
+                value={userEmail}
+                onChange={event => setUserEmail(event.target.value)}
+              />
+            </div>
+
+            <div className="form-group mb-2">
+              <select
+                type="text"
+                className="form-control"
+                placeholder="Permiso"
+                name="role"
+                value={role}
+                onChange={event => setRole(event.target.value)}
+              >
+                <option defaultValue>Selecciona el role</option>
+                <option value="Vendedor">Vendedor</option>
+                <option value="Encargado">Encargado</option>
+                <option value="Administrador">Administrador</option>
+              </select>
+            </div>
+
+            <div className="d-grid gap-2">
+              <input
+                type="submit"
+                className="btnSubmit"
+                value="Actualiza Usuario"
+              />
+            </div>
+          </form>{" "}
+        </form>
+    </div>
+  );
+};
