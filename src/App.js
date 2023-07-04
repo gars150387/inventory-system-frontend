5dde8ed4ef3c9c84ee9793e395a6a2f8be47def4
gars150387 <gar.santeliz@gmail.com>
2023-07-04 17:43:33 -0400

    new navbar behavior

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import { RouteMain } from "./routes/Route";
 import { Navbar } from "./components/ui/Navbar";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { adminUser } = useSelector((state) => state.admin);
   return (
     <div className="App">
-      <Navbar />
+      {adminUser.token && <Navbar />}
       <RouteMain />
     </div>
   );
