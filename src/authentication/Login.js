c1f800a056a7ca0bdbd3b34d33fbe76a14075636
gars150387 <gar.santeliz@gmail.com>
2023-07-04 17:41:25 -0400

    new login component styled

--- src/authentication/Login.js
+++ src/authentication/Login.js
@@ -1,31 +1,34 @@
 import { apiBase } from "../components/api/Api";
-import { useForm } from "../hooks/useForm";
-import "../style/component/Login.css";
 import { useDispatch } from "react-redux";
 import { onAddNewAdminUser } from "../store/slices/adminSlice";
 import { useNavigate } from "react-router";
-import Swal from "sweetalert2";
-
-const loginFormFields = {
-  loginEmail: "",
-  loginPassword: "",
-};
-
+import { Typography, FormLabel, Grid, OutlinedInput } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { notification } from "antd";
+import "./Login.css"
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const {
-    loginEmail,
-    loginPassword,
-    onInputChange: onloginInputChange,
-  } = useForm(loginFormFields);
+  const [api, contextHolder] = notification.useNotification();
 
-  const onSubmitLogin = async (event) => {
-    event.preventDefault();
+  const openNotification = (message) => {
+    api.error({
+      message: `Mensaje de error`,
+      description: `${message.response.data.msg}`,
+      placement: "topRight",
+      duration: 0
+    });
+  };
+  const onSubmitLogin = async (data) => {
     try {
       const response = await apiBase.post("/admin/login", {
-        email: loginEmail.toLowerCase(),
-        password: loginPassword,
+        email: data.email,
+        password: data.password
       });
 
       if (response) {
@@ -38,48 +41,143 @@ export const Login = () => {
           email: response.data.email,
           token: response.data.token,
           iud: response.data.uid,
-          role: response.data.role,
+          role: response.data.role
         })
       );
     } catch (error) {
-      console.log(
-        "🚀 ~ file: Login.js ~ line 29 ~ onSubmitLogin ~ error",
-        error
-      );
-      Swal.fire("Error", "Usuario/Contrasena no coinciden", "error")
+      openNotification(error);
     }
   };
 
   return (
-    <div className="container login-container">
-        <div className="login-form-1">
-          <h3>Sign In</h3>
-          <form onSubmit={onSubmitLogin}>
-            <div className="form-group mb-2">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Email"
-                name="loginEmail"
-                value={loginEmail}
-                onChange={onloginInputChange}
+    <>
+      {contextHolder}
+
+      <Grid
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        item
+        xs={6}
+      >
+        <Grid
+          container
+          display={"flex"}
+          flexDirection={"column"}
+          justifyContent={"space-around"}
+          alignItems={"center"}
+        >
+          <Grid
+            display={"flex"}
+            flexDirection={"column"}
+            justifyContent={"space-around"}
+            alignItems={"center"}
+            className="login-container"
+            container
+          >
+            <Typography
+              style={{
+                color: "var(--gray900, #101828)",
+                fontSize: "30px",
+                fontFamily: "Inter",
+                fontWeight: "600",
+                lineHeight: "38px"
+              }}
+              variant="h1"
+            >
+              Bienvenido
+            </Typography>{" "}
+            <Typography
+              style={{
+                color: "var(--gray-500, #667085)",
+                fontSize: "16px",
+                fontFamily: "Inter",
+                lineHeight: "24px"
+              }}
+            >
+              Por favor ingrese su correo electronico
+            </Typography>
+            <form
+              style={{ padding: "30px" }}
+              onSubmit={handleSubmit(onSubmitLogin)}
+            >
+              <Grid marginX={0} marginY={5} textAlign={"left"} item xs={12}>
+                <FormLabel style={{ marginBottom: "0.5rem" }}>Email</FormLabel>
+                <OutlinedInput
+                  {...register("email", { required: true, minLength: 10 })}
+                  aria-invalid={errors?.email ? true : false}
+                  style={{
+                    borderRadius: "12px",
+                    border: `${errors?.email && "solid 1px #004EEB"}`
+                  }}
+                  placeholder="Enter your email"
+                  fullWidth
+                />
+                {errors?.email && (
+                  <Typography>El correo es requerido</Typography>
+                )}
+              </Grid>
+              <Grid marginX={0} textAlign={"left"} item xs={12}>
+                <FormLabel style={{ marginBottom: "0.5rem" }}>
+                  Password
+                </FormLabel>
+                <OutlinedInput
+                  {...register("password", { required: true, minLength: 6 })}
+                  style={{ borderRadius: "12px" }}
+                  placeholder="******"
+                  type="password"
+                  fullWidth
+                />
+                {errors?.password?.message}
+              </Grid>
+              <Grid
+                marginY={"20px"}
+                marginX={0}
+                textAlign={"left"}
+                display={"flex"}
+                justifyContent={"space-between"}
+                alignItems={"center"}
+                item
+                xs={12}
+              >
+                <Grid
+                  item
+                  xs={6}
+                  display={"flex"}
+                  justifyContent={"flex-start"}
+                  alignItems={"center"}
+                >
+                  <Typography
+                    style={{
+                      color: "#004EEB",
+                      fontSize: "14px",
+                      fontFamily: "Inter",
+                      fontWeight: "600",
+                      lineHeight: "20px",
+                      cursor: "pointer"
+                    }}
+                  >
+                    Olvido contrasena?
+                  </Typography>
+                </Grid>
+              </Grid>
+              <OutlinedInput
+                style={{
+                  color: "#fff",
+                  width: "100%",
+                  border: "1px solid var(--blue-dark-600, #155EEF)",
+                  background: " var(--blue-dark-600, #155EEF)",
+                  borderRadius: "8px",
+                  boxShadow: "0px 1px 2px 0px rgba(16, 24, 40, 0.05)"
+                }}
+                type="submit"
+                value="Sign in"
               />
-            </div>
-            <div className="form-group mb-2">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-                name="loginPassword"
-                value={loginPassword}
-                onChange={onloginInputChange}
-              />
-            </div>
-            <div className="d-grid gap-2">
-              <input type="submit" className="btnSubmit" value="Login" />
-            </div>
-          </form>
-        </div>
-    </div>
+            </form>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
   );
 };
