99c9d3e03576cc29a7b4a4adf333e042cc566081
gars150387 <gar.santeliz@gmail.com>
2023-07-08 18:05:11 -0400

    redefined component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ import '../style/page/Home.css'
 export const Home = () => {
   return (
     <div className='container-home-page'>
-        <h1>Home</h1>
         <div>
             <Login />
         </div>
