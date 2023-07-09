6d75321dc02c5863f240c6a199e0f3de95077add
gars150387 <gar.santeliz@gmail.com>
2023-07-08 22:44:18 -0400

    remove reduce function for cartRef

--- src/store/slices/orderSlice.js
+++ src/store/slices/orderSlice.js
@@ -1,16 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const order = [];
-
 const orderSlice = createSlice({
   name: "order",
   initialState: {
-    order
+    order: [],
+    ref: [],
   },
   reducers: {
     onAddToOrder: (state, { payload }) => {
       let index = 0;
       state.order.splice(index, 0, payload);
+      index++
+    },
+    onAddRef: (state, { payload }) => {
+      let index = 0;
+      state.ref.splice(index, 0, payload);
       index++
     },
     onResetCart:(state)=>{
@@ -28,4 +32,4 @@ const orderSlice = createSlice({
 export default orderSlice.reducer;
 
 // Action creators are generated for each case reducer function
-export const { onAddToOrder, onResetCart, onDeleteItemInCart, onEditItemInOrder } = orderSlice.actions;
+export const { onAddToOrder, onResetCart, onDeleteItemInCart, onEditItemInOrder, onAddRef } = orderSlice.actions;
