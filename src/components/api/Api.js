88b0394dd6b09969230480e26c1d98dce7213f5a
gars150387 <gar.santeliz@gmail.com>
2023-09-16 01:17:23 -0400

    new api url

--- src/components/api/Api.js
+++ src/components/api/Api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-
+//https://rosa-mistica-backend-c56l-dev.fl0.io
+//https://rosa-mistica-app-backend.herokuapp.com
 export const apiBase = axios.create({
-  baseURL: "https://rosa-mistica-app-backend.herokuapp.com/api",
+  baseURL: "https://rosa-mistica-backend-c56l-dev.fl0.io/api",
 });
 
 //TODO: config interceptors
